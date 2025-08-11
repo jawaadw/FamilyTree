66b5301e8a6bddc95277ae599f443138a64bfdfa
Jawaad <jawaadw@gmail.com>
2025-08-10 20:38:22 -0400

    initial commit

---
+++ apps/web/src/routes/dashboard/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { authClient } from '$lib/auth-client';
+	import { get } from 'svelte/store';
+
+	const sessionQuery = authClient.useSession();
+
+
+	onMount(() => {
+		const { data: session, isPending } = get(sessionQuery);
+		if (!session && !isPending) {
+			goto('/login');
+		}
+	});
+</script>
+
+{#if $sessionQuery.isPending}
+	<div>Loading...</div>
+{:else if !$sessionQuery.data}
+{:else}
+	<div>
+		<h1>Dashboard</h1>
+		<p>Welcome {$sessionQuery.data.user.name}</p>
+	</div>
+{/if}
